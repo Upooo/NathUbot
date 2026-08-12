ed75e839c30ee049228f15043f14c37133be2cbf
upoo <aryasptnanggra@gmail.com>
2026-08-12 16:47:02 +0700

    feat: add modules (pertanyaan_JEMBUT, ping, profiles, purge, react, salam, savecontak, spamg)

---
+++ GooUbot/modules/salam.py
@@ -0,0 +1,55 @@
+import asyncio
+from time import sleep
+from GooUbot import *
+
+__MODULE__ = "salam"
+__HELP__ = """
+<blockquote><b>--ʙᴀɴᴛᴜᴀɴ ᴜɴᴛᴜᴋ sᴀʟᴀᴍ--</b></blockquote>
+
+<blockquote><b>🚦 ᴘᴇʀɪɴᴛᴀʜ :</b> <code>{0}p</code>
+🦠 ᴋᴇᴛ : ᴍᴇɴɢɪʀɪᴍ ᴀssᴀʟᴀᴍᴜ'ᴀʟᴀɪᴋᴜᴍ.</blockquote>
+<blockquote><b>🚦 ᴘᴇʀɪɴᴛᴀʜ :</b> <code>{0}pe</code>
+🦠 ᴋᴇᴛ : ᴍᴇɴɢɪʀɪᴍ ᴀssᴀʟᴀᴍᴜ'ᴀʟᴀɪᴋᴜᴍ ᴡᴀʀᴀʜᴍᴀᴛᴜʟʟᴀʜɪ ᴡᴀʙᴀʀᴀᴋᴀᴛᴜʜ.</blockquote>
+<blockquote><b>🚦 ᴘᴇʀɪɴᴛᴀʜ :</b> <code>{0}l</code>
+🦠 ᴋᴇᴛ : ᴍᴇɴɢɪʀɪᴍ ᴡᴀ'ᴀʟᴀɪᴋᴜᴍsᴀʟᴀᴍ.</blockquote>
+<blockquote><b>🚦 ᴘᴇʀɪɴᴛᴀʜ :</b> <code>{0}wl</code>
+🦠 ᴋᴇᴛ : ᴍᴇɴɢɪʀɪᴍ ᴡᴀ'ᴀʟᴀɪᴋᴜᴍsᴀʟᴀᴍ ᴡᴀʀᴀʜᴍᴀᴛᴜʟʟᴀʜɪ ᴡᴀʙᴀʀᴀᴋᴀᴛᴜʜ.</blockquote>
+<blockquote><b>🚦 ᴘᴇʀɪɴᴛᴀʜ :</b> <code>{0}as</code>
+🦠 ᴋᴇᴛ : ᴍᴇɴɢɪʀɪᴍ ᴛᴇᴋs ᴘᴇɴᴄᴏʙᴀᴀɴ.</blockquote>
+"""
+
+
+
+@PY.UBOT("p")
+async def inijugajangandiapusataudigantikrnizzyganteng(client, message):
+    await message.edit(
+        "Assalamu'alaikum",
+    )
+
+
+@PY.UBOT("pe")
+async def biarpanjangajayangpentingizzyganteng(client, message):
+    await message.edit(
+        "Assalamualaikum Warahmatullahi Wabarakatuh",
+    )
+
+
+@PY.UBOT("l")
+async def biarmampuslusemuakontol(client, message):
+    await message.edit(
+        "waalaikumsalam",
+    )
+
+
+@PY.UBOT("wl")
+async def ularnagapanajnagnyabukankepalangtapiizzygantengamat(client, message):
+    await message.edit(
+        "Wa'alaikumsalam Warahmatullahi Wabarakatuh",
+    )
+
+
+@PY.UBOT("as")
+async def pelerpelerpeler(client, message):
+    await message.edit(
+        "Salam dulu woy!",
+    )
---
+++ GooUbot/modules/spamg.py
@@ -0,0 +1,89 @@
+import asyncio
+
+from pyrogram.enums import ChatType
+from pyrogram.errors import FloodWait
+
+from .. import *
+from GooUbot import *
+
+__MODULE__ = "spam"
+__HELP__ = """
+<blockquote><b>--ʙᴀɴᴛᴜᴀɴ ᴜɴᴛᴜᴋ sᴘᴀᴍ--</b></blockquote>
+
+<blockquote><b>🚦 ᴘᴇʀɪɴᴛᴀʜ :</b> <code>{0}spam</code>
+🦠 ᴋᴇᴛ : ᴍᴇʟᴀᴋᴜᴋᴀɴ sᴘᴀᴍ ᴘᴇsᴀɴ ʙᴇʀᴜʀᴜᴛᴀɴ.</blockquote>
+<blockquote><b>🚦 ᴘᴇʀɪɴᴛᴀʜ :</b> <code>{0}setdelay</code>
+🦠 ᴋᴇᴛ : ᴍᴇɴɢᴀᴛᴜʀ ᴅᴇʟᴀʏ ᴀɴᴛᴀʀᴀ ᴘᴇsᴀɴ ʏᴀɴɢ ᴅɪᴋɪʀɪᴍ.</blockquote>
+<blockquote><b>🚦 ᴘᴇʀɪɴᴛᴀʜ :</b> <code>{0}stopspam</code>
+🦠 ᴋᴇᴛ : ᴍᴇᴍʙᴇʀʜᴇɴᴛɪᴋᴀɴ ᴘʀᴏsᴇs sᴘᴀᴍ ʏᴀɴɢ sᴇᴅᴀɴɢ ʙᴇʀʟᴀɴɢsᴜɴɢ.</blockquote>
+"""
+
+
+spam_progress = []
+
+async def SpamMsg(client, message, send):
+    delay = await get_vars(client.me.id, "SPAM") or 0
+    await asyncio.sleep(int(delay))
+    if message.reply_to_message:
+        await send.copy(message.chat.id)
+    else:
+        await client.send_message(message.chat.id, send)
+
+@PY.UBOT("spam")
+@PY.TOP_CMD
+async def _(client, message):
+    global spam_progress
+    spam_progress.append(client.me.id)
+    sks = await EMO.BERHASIL(client)
+    _msg = "<b>memproses...</b>"
+
+    r = await message.reply(_msg)
+    count, msg = extract_type_and_msg(message)
+
+    try:
+        count = int(count)
+    except Exception:
+        return await r.edit(f"<b><code>{message.text.split()[0]}</code> [jumlah] [text/reply_msg]</b>")
+
+    if not msg:
+        return await r.edit(
+            f"<b><code>{message.text.split()[0]}</code> [jumlah] [text/reply_msg]</b>"
+        )
+    
+    for _ in range(count):
+        if client.me.id not in spam_progress:
+            await r.edit(f"<blockquote><b>proses spam berhasil di batalkan !</b> {sks}</blockquote>")
+            return
+        await SpamMsg(client, message, msg)
+
+    spam_progress.remove(client.me.id)    
+    await r.edit("<b>spam telah selesai</b>")
+
+@PY.UBOT("setdelay")
+@PY.TOP_CMD
+async def _(client, message):
+    _msg = "<b>memproses...</b>"
+
+    r = await message.reply(_msg)
+    count, msg = extract_type_and_msg(message)
+
+    try:
+        count = int(count)
+    except Exception:
+        return await r.edit(f"<b><code>{message.text.split()[0]}</code> [count]</b>")
+
+    if not count:
+        return await r.edit(f"<b><code>{message.text.split()[0]}</code> [count]</b>")
+
+    await set_vars(client.me.id, "SPAM", count)
+    return await r.edit("<b>spam delay berhasil di setting</b>")
+
+@PY.UBOT("stopspam")
+@PY.TOP_CMD
+async def _(client, message):
+    global spam_progress
+    if client.me.id in spam_progress:
+        spam_progress.remove(client.me.id)
+        await message.reply("<b>spam telah berhenti</b>")
+    else:
+        await message.reply("<b>tidak ada spam yang ditemukan</b>")
